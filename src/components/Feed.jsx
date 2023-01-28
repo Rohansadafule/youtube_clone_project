6478bba9915bd4150c7ee4ab0d1d5621d89b0c09
Rohansadafule <sadafulerohan45@gmail.com>
2023-01-28 20:41:15 +0530

    configure API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { SideBar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
 
-export default Feed
+  const [selectedCategory, setselectedCategory] = useState("New")
+  const [videos, setvideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data)=> setvideos(data.items))
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar selectedCategory={selectedCategory} setselectedCategory={setselectedCategory}/>
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 React App
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h5" fontWeight="bold" sx={{ color: "white" }}>
+          {selectedCategory}
+          <span style={{ color: "#F31503" }}> Videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
